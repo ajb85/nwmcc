5d9539487cbd2a1804020636aec815aeeafa64d9
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 02:19:59 -0400

    Adds action creator to populate user account if a token exists

--- src/reducers/account.js
+++ src/reducers/account.js
@@ -1,12 +1,25 @@
+import axios from 'axios';
+
 const initialState = {
   id: null,
   email: '',
-  nickname: ''
+  nickname: '',
+  token: ''
 };
+
+const SET_ACCOUNT_INFO = 'ACCOUNT/INFO/SET';
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_ACCOUNT_INFO:
+      return { ...state, ...action.payload };
     default:
       return state;
   }
+};
+
+export const getAccountInfo = () => dispatch => {
+  axios
+    .get('/account')
+    .then(({ data }) => dispatch({ type: SET_ACCOUNT_INFO, payload: data }));
 };
