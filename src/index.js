bf161f087e860efa7d024a7929871dce7a497b86
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 01:33:17 -0400

    Adds redux & routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { Provider } from 'react-redux';
+import store from './store.js';
+
+// Routing
+import { Router } from 'react-router-dom';
+import history from './history.js';
+
+// Axios interceptors
+import './interceptors/';
+
+import App from './App.js';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
