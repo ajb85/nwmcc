bba24493ddc42d75e3947cd8128b8c3a55d0d685
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 02:20:28 -0400

    Adds basic ACs to determine if the app is loading and if the backend is awake

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,11 +1,44 @@
+import axios from 'axios';
+
 const initialState = {
-  backendAwake: null,
+  isAwake: null,
   isLoading: false
 };
 
+const APP_LOADING = 'APP/LOADING';
+const APP_DONE_LOADING = 'APP/LOADING/DONE';
+const BACKEND_SLEEPING = 'APP/BACKEND/SLEEP';
+const BACKEND_AWAKE = 'APP/BACKEND/AWAKE';
+
 export default (state = initialState, action) => {
   switch (action.type) {
+    case APP_LOADING:
+      return { ...state, isLoading: true };
+    case APP_DONE_LOADING:
+      return { ...state, isLoading: false };
+    case BACKEND_SLEEPING:
+      return { ...state, isAwake: false };
+    case BACKEND_AWAKE:
+      return { ...state, isAwake: true };
     default:
       return state;
   }
+};
+
+export const loading = () => ({ type: APP_LOADING });
+export const doneLoading = () => ({ type: APP_DONE_LOADING });
+
+export const getBackendPulse = () => dispatch => {
+  const interval = setTimeout(() => {
+    dispatch({ type: BACKEND_SLEEPING });
+    clearInterval(interval);
+  }, 1000);
+
+  axios.get('/').then(_ => {
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    dispatch({ type: BACKEND_AWAKE });
+  });
 };
