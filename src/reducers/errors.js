f14890a36810e583de5bd7226933e7bcefe4272c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 13:14:45 -0400

    Creates ACs to log errors and clear them

--- src/reducers/errors.js
+++ src/reducers/errors.js
@@ -5,9 +5,27 @@ const initialState = {
   }
 };
 
+const LOG_ERROR = 'ERRORS/LOG';
+const CLEAR_ERRORS = 'ERRORS/CLEAR';
+
 export default (state = initialState, action) => {
   switch (action.type) {
+    case LOG_ERROR: {
+      return {
+        ...state,
+        [action.payload.section]: {
+          ...state[action.payload.section],
+          [action.payload.subsection]: action.payload.error
+        }
+      };
+    }
+    case CLEAR_ERRORS:
+      return initialState;
     default:
       return state;
   }
 };
+
+export const logError = error => ({ type: LOG_ERROR, payload: error });
+
+export const clearErrors = () => ({ type: CLEAR_ERRORS });
