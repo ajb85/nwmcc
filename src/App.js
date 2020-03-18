db21c3c4c823b6a75cbbc87afeea28dedfc46709
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 02:19:21 -0400

    Shows status of the backend via text display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getBackendPulse } from 'reducers/app.js';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const dispatch = useDispatch();
+  const { isAwake } = useSelector(state => ({ isAwake: state.app.isAwake }));
+
+  useEffect(() => {
+    if (isAwake === null) {
+      dispatch(getBackendPulse());
+    }
+  }, [isAwake, dispatch]);
+
+  const getText = () => {
+    switch (isAwake) {
+      case null:
+        return 'Loading app!';
+      case false:
+        return 'Oops, the backend is sleeping!';
+      default:
+        return 'Welcome';
+    }
+  };
+  return <div className="App">{getText()}</div>;
 }
 
 export default App;
