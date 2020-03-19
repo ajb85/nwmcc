b4f1479a14146b2f3a23c1b6863f9e03d658d9bc
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 22:56:42 -0400

    Has method to send messages

--- src/components/Chat/helpers/Interface.js
+++ src/components/Chat/helpers/Interface.js
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from '../styles.module.scss';
 
 function Interface(props) {
-  const [message, setMessage] = useState('');
+  const [content, setContent] = useState('');
+  const { activeChat } = useSelector(state => ({
+    activeChat: state.chats.activeChat
+  }));
+
+  const sendMessage = e => {
+    e.preventDefault();
+    if (content.length) {
+      axios
+        .post(`/chats/${activeChat}/message`, { content })
+        .then(_ => setContent(''));
+    }
+  };
 
   const handleMessageChange = e => {
     const limit = parseInt(process.env.REACT_APP_CHAR_LIMIT, 10);
     if (e.target.value.length <= limit) {
-      setMessage(e.target.value);
+      setContent(e.target.value);
     }
   };
   return (
-    <form className={styles.Interface}>
-      <textarea value={message} onChange={e => handleMessageChange(e)} />
+    <form className={styles.Interface} onSubmit={e => sendMessage(e)}>
+      <input value={content} onChange={e => handleMessageChange(e)} />
       <div className={styles.buttonContainer}>
-        <FontAwesomeIcon icon={['fal', 'paper-plane']} />
-        <button type="submit" style={{ display: 'none' }} />
+        <button type="submit">
+          <FontAwesomeIcon icon={['fal', 'paper-plane']} />
+        </button>
       </div>
     </form>
   );
