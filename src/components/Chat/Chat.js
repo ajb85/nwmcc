b702d2824aa2988be222f313b97a7c6906f5b09f
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-19 00:02:10 -0400

    Fetches chats upon mount

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,23 +1,31 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Log from './helpers/Log.js';
 import UserList from './helpers/UserList.js';
 import Interface from './helpers/Interface.js';
 
 import sockets from 'sockets/';
+import { populateChats } from 'reducers/chats.js';
 
 import styles from './styles.module.scss';
 
 function Chat(props) {
+  const dispatch = useDispatch();
   const { chatRooms } = useSelector(state => ({
     chatRooms: state.chats.rooms
   }));
   useEffect(() => {
-    Object.keys(chatRooms).forEach(chat_id => {
-      sockets.join(chat_id);
-    });
-  }, [chatRooms]);
+    const chat_ids = Object.keys(chatRooms);
+
+    if (!chat_ids.length) {
+      dispatch(populateChats());
+    } else {
+      chat_ids.forEach(chat_id => {
+        sockets.join(chat_id);
+      });
+    }
+  }, [chatRooms, dispatch]);
   return (
     <section className={styles.Chat}>
       <div className={styles.top}>
