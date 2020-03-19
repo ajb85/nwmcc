763743b8c1937e7e005515bcbefa4fa41a39571f
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-19 02:31:17 -0400

    Imports userlist

--- src/sockets/listeners/defaults/index.js
+++ src/sockets/listeners/defaults/index.js
@@ -1,5 +1,6 @@
 import error from './error.js';
 import connect from './connect.js';
 import disconnect from './disconnect.js';
+import userlist from './userlist.js';
 
-export default { error, connect, disconnect };
+export default { error, connect, disconnect, userlist };
