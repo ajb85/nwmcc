8d9beecc55b8e40cdc76d1d2ef08a21b2610b4f3
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-19 02:29:55 -0400

    Renders a list of provided users from sockets

--- src/components/Chat/helpers/UserList.js
+++ src/components/Chat/helpers/UserList.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from '../styles.module.scss';
 
 function UserList(props) {
-  return <div className={styles.UserList}>Hello UL World</div>;
+  const { users } = useSelector(state => ({ users: state.chats.users }));
+
+  if (!users.length) {
+    return <p>Loading...</p>;
+  }
+  const test = [
+    'asdlfkjsdflskjfslfkjsdfldskjsdflkjsdflkdsjfdslkfjsdlfkjdlkdfjsldkfjsldkfsjdkl'
+  ];
+  return (
+    <div className={styles.UserList}>
+      {test.map(nickname => (
+        <p>{nickname}</p>
+      ))}
+    </div>
+  );
 }
 
 export default UserList;
