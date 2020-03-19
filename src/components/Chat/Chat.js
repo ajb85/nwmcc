48e6151714b58c6992ffb1058df0e2caf5fcb662
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 22:56:28 -0400

    Subscribes to all chats upon mounting

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import Log from './helpers/Log.js';
 import UserList from './helpers/UserList.js';
 import Interface from './helpers/Interface.js';
 
+import sockets from 'sockets/';
+
 import styles from './styles.module.scss';
 
 function Chat(props) {
+  const { chatRooms } = useSelector(state => ({
+    chatRooms: state.chats.rooms
+  }));
+  useEffect(() => {
+    Object.keys(chatRooms).forEach(chat_id => {
+      sockets.join(chat_id);
+    });
+  }, [chatRooms]);
   return (
     <section className={styles.Chat}>
       <div className={styles.top}>
