29f9a30a77331caa34b4c25558eab9603d3e811d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-18 22:59:58 -0400

    Adds chats

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import account from './account.js';
 import app from './app.js';
-import chat from './chat.js';
+import chats from './chats.js';
 import errors from './errors.js';
 
 export default combineReducers({
   account,
   app,
-  chat,
+  chats,
   errors
 });
