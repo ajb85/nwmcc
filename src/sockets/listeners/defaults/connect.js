8e292bf7a8c36d0d4b697f815c93adb286abd343
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-19 00:03:53 -0400

    Updates to use subscribe method instead of emit

--- src/sockets/listeners/defaults/connect.js
+++ src/sockets/listeners/defaults/connect.js
@@ -8,14 +8,13 @@ export default function() {
   console.log('Connected to socket manager');
   if (!this.identified) {
     // Identify again to the backend
-    console.log(3);
     this._identify();
   }
 
   if (Object.keys(this.subscribed).length) {
     // Resub to all subscriptions
     for (let room in this.subscribed) {
-      this.emit('subscribe', room);
+      this.subscribe(room);
     }
   }
 }
